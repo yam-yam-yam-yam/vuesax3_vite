276d55976613eda556be4947cdfb860da661edf3
yam-yam-yam-yam <77680103+yam-yam-yam-yam@users.noreply.github.com>
2022-08-04 16:07:08 +0900

    2022-08-04/vs-checkbox

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,16 @@ import { ref } from 'vue'
 import AlertComponent from "./components/Alert.vue";
 import AvatarComponent from "./components/Avatar.vue";
 import ButtonComponent from "./components/Button.vue"
+import CardComponent from "./components/Card.vue"
+import CheckboxComponent from "./components/Checkbox.vue"
 import LoadlingComponent from "./components/Loadling.vue";
 
 const components = [
   AlertComponent,
   AvatarComponent,
   ButtonComponent,
+  CardComponent,
+  CheckboxComponent,
   LoadlingComponent
 ]
 const currentComponent = ref(components[0])
---
+++ src/components/Checkbox.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { ref } from 'vue'
+
+const data1 = ref(true);
+const data2 = ref(undefined);
+const data3 = ref([]);
+</script>
+
+<template>
+    <vs-card>
+        <vs-checkbox v-model="data1"></vs-checkbox>
+        <p>v-model을 주어 작동</p>
+        <p>아이콘의 높이가 체크박스 칸을 넘어가서 height를 기본값으로 커스텀함</p>
+        <p>vuesax4와 같이 border-radius와 font-size를 커스텀함</p>
+    </vs-card>
+
+    <vs-card>
+        <vs-checkbox v-model="data1" color="success"></vs-checkbox>
+        <p>color적용 가능</p>
+    </vs-card>
+
+    <vs-card>
+        <vs-checkbox v-model="data2" vs-value="사과">사과</vs-checkbox>
+        <p>string값 checkbox</p>
+        <p>{{`${data2}`}}</p>
+    </vs-card>
+
+    <vs-card>
+        <vs-checkbox v-model="data3" vs-value="사과">사과</vs-checkbox>
+        <vs-checkbox v-model="data3" vs-value="귤">귤</vs-checkbox>
+        <p>string값 checkbox</p>
+        <p>{{`${data3}`}}</p>
+    </vs-card>
+
+    <vs-card>
+        <vs-checkbox v-model="data" icon="close"></vs-checkbox>
+        <p>icon 설정 가능</p>
+    </vs-card>
+</template>
+
+<style scoped>
+
+</style>
+
+<style>
+.vs-checkbox {
+    width:15px !important;
+    height:15px !important;
+    border-radius: 30% !important;
+    padding: 2.2% !important;
+}
+.vs-checkbox--check {
+    height: initial !important;
+}
+.vs-checkbox--icon {
+    font-size:16px !important;
+}
+</style>
